ad966096e54ea0b0916c11760573ec1a53ae8f47
Humdinger <humdinger@mailbox.org>
2023-12-09 09:43:19 +0100

    Remove input_server restart from un/install.sh
    Not needed any more as input devices are un/loaded when copied
    in the right add-on folders. Plus, a quit input_server doesn't
    seem to restart sucessfully, forcing a reboot...

--- install.sh
+++ install.sh
@@ -5,7 +5,3 @@ rm -f ~/config/non-packaged/add-ons/input_server/filters/KeyCursorFilter
 
 cp src/KeyCursor ~/config/non-packaged/add-ons/input_server/devices/KeyCursorDevice
 ln -s ~/config/non-packaged/add-ons/input_server/devices/KeyCursorDevice ~/config/non-packaged/add-ons/input_server/filters/KeyCursorFilter
-
-# /system/servers/input_server -q
-# kill -9 `pidof input_server`
-# sleep 1
