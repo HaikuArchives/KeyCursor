45a6b0a11dc09fbb3e84754183658fdc951f9342
Humdinger <humdinger@mailbox.org>
2023-12-16 11:03:24 +0100

    Put help text into AboutWindow
    As the app doesn't really need much explanation, put a little
    help text into the about window, instead of adding an extra
    help window.

--- src/AboutHelpWin.cpp
+++ src/AboutHelpWin.cpp
@@ -30,7 +30,12 @@ ShowAbout()
 	aboutwindow->AddCopyright(2004, "Oscar Lesta");
 	aboutwindow->AddAuthors(authors);
 	aboutwindow->AddDescription(B_TRANSLATE(
-		"KeyCursor lets you control the mouse pointer with the keyboard."));
+		"KeyCursor lets you control the mouse pointer with the keyboard.\n"
+		"In the preference window, you choose the modifier key combination "
+		"that will toggle control on/off.\n"
+		"When toggled on, the cursor keys will move the mouse pointer.\n"
+		"Hit SPACE for a left-click, SHIFT+SPACE for a right-click."
+		));
 
 	const char* thanks[] = {
 		B_TRANSLATE("Nathan Schrenk for his BeNewsletter article"),
@@ -38,5 +43,15 @@ ShowAbout()
 		NULL
 	};
 	aboutwindow->AddSpecialThanks(thanks);
+
+	// Approximate nice looking window size
+	font_height fh;
+	BFont font;
+	font.GetHeight(&fh);
+	float height = static_cast<float>(ceil(fh.ascent + fh.descent + fh.leading));
+	float width = font.StringWidth(
+		"KeyCursor lets you control the mouse pointer with the keyboard.") * 1.3;
+
+	aboutwindow->ResizeTo(width, height * 22);
 	aboutwindow->Show();
 }
